78c9f568ca2872900ee4c07ac3ceced4368f0126
KemonoElite <97435466+KemonoElite@users.noreply.github.com>
2025-06-01 15:37:16 -0500

    Add files via upload

---
+++ sources.nix
@@ -0,0 +1,22 @@
+{
+  sf-pro = {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
+    hash = "sha256-Mu0pmx3OWiKBmMEYLNg+u2MxFERK07BQGe3WAhEec5Q=";
+  };
+  sf-compact = {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
+    hash = "sha256-Mkf+GK4iuUhZdUdzMW0VUOmXcXcISejhMeZVm0uaRwY=";
+  };
+  sf-mono = {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg";
+    hash = "sha256-tZHV6g427zqYzrNf3wCwiCh5Vjo8PAai9uEvayYPsjM=";
+  };
+  sf-arabic = {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Arabic.dmg";
+    hash = "sha256-Vvfl9ByKww55lE3RTIx4TOEfuyVkENCMbHLYFGLhp2o=";
+  };
+  ny = {
+    url = "https://devimages-cdn.apple.com/design/resources/download/NY.dmg";
+    hash = "sha256-tn1QLCSjgo5q4PwE/we80pJavr3nHVgFWrZ8cp29qBk=";
+  };
+}
